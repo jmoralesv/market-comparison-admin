1148ebf3c45d6ef869a1ab1ea92c587c5d2f18c8
jmoralesv <korum03@gmail.com>
2023-10-07 20:53:25 -0500

    Refactoring the methods for configuring the services and the pipeline middlewares.

---
+++ src/Market.Comparison.Admin/HostingExtensions.cs
@@ -0,0 +1,66 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace Market.Comparison.Admin;
+
+internal static class HostingExtensions
+{
+    internal static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddHttpClient();
+
+        JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+        builder.Services
+            .AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://localhost:7100";
+
+                options.ClientId = "web";
+                options.ClientSecret = "secret";
+                options.ResponseType = "code";
+
+                options.Scope.Clear();
+                options.Scope.Add("openid");
+                options.Scope.Add("profile");
+                options.Scope.Add("verification");
+                options.Scope.Add("Market.Comparison.Api");
+                options.Scope.Add("offline_access");
+                options.ClaimActions.MapJsonKey("email_verified", "email_verified");
+                options.GetClaimsFromUserInfoEndpoint = true;
+                options.SaveTokens = true;
+            });
+
+        return builder.Build();
+    }
+
+    internal static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapRazorPages()
+            .RequireAuthorization();
+
+        return app;
+    }
+}
