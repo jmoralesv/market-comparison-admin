78538f6d37a5b83f5f3e8d86f6648923a5cdcc5e
Jorge Morales <korum03@gmail.com>
2023-12-04 22:47:37 -0500

    Upgrade to .NET 8 + new YAML changes + new editorconfig. (#5)

--- src/Market.Comparison.Admin/Pages/CallApi.cshtml.cs
+++ src/Market.Comparison.Admin/Pages/CallApi.cshtml.cs
@@ -5,25 +5,20 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Market.Comparison.Admin.Pages;
 
-public class CallApiModel : PageModel
+public class CallApiModel(HttpClient httpClient) : PageModel
 {
-    private readonly HttpClient _httpClient;
-
-    public CallApiModel(HttpClient httpClient)
-    {
-        _httpClient = httpClient;
-    }
+    private static readonly JsonSerializerOptions _jsonSerializerOptions = new() { WriteIndented = true };
 
     public string Json { get; set; } = string.Empty;
 
     public async Task OnGetAsync()
     {
         var accessToken = await HttpContext.GetTokenAsync("access_token");
-        _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-        var content = await _httpClient.GetStringAsync("https://localhost:7101/weather-forecast");
+        httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        var content = await httpClient.GetStringAsync("https://localhost:7101/weather-forecast");
 
         var parsed = JsonDocument.Parse(content);
-        var formatted = JsonSerializer.Serialize(parsed, new JsonSerializerOptions { WriteIndented = true });
+        var formatted = JsonSerializer.Serialize(parsed, _jsonSerializerOptions);
 
         Json = formatted;
     }
